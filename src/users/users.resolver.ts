b31cb838d18cf2d650d2d670898d8d04ea0c96b2
kimhe5623 <kimhe5623@naver.com>
2021-01-13 01:44:59 +0900

    userProfile completed

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,20 +1,16 @@
 import { UseGuards } from "@nestjs/common";
 import { Args, Context, Mutation, Query, Resolver } from "@nestjs/graphql";
 import { AuthUser } from "src/auth/auth-user.decorator";
-import { AuthGuard } from "src/auth/auth.guart";
+import { AuthGuard } from "src/auth/auth.guard";
 import { CreateAccountInput, CreateAccountOutput } from "./dtos/create-account.dto";
 import { LoginInput, LoginOutput } from "./dtos/login.dto";
+import { UserProfileInput, UserProfileOutput } from "./dtos/user-profile.dto";
 import { User } from "./entities/user.entity";
 import { UsersService } from "./users.service"
 
 @Resolver(of => User)
 export class UserResolver {
     constructor(private readonly usersService: UsersService) {}
-
-    @Query(returns => Boolean)
-    hi() {
-        return false;
-    }
 
     @Mutation(returns => CreateAccountOutput)
     async createAccount(
@@ -41,11 +37,30 @@ export class UserResolver {
            }
     }
 
-    @Query(returns => User)
     @UseGuards(AuthGuard)
+    @Query(returns => User)
     me(@AuthUser() authUser) {
-        console.log(authUser);
         return authUser;
     }
     
+
+    @UseGuards(AuthGuard)
+    @Query(returns => UserProfileOutput) 
+    async userProfile(@Args() userProfileInput: UserProfileInput): Promise<UserProfileOutput> {
+        try {
+            const user = await this.usersService.findById(userProfileInput.userId);
+            if(!user) {
+                throw Error();
+            }
+            return {
+                ok: true, // If user found, it will be true or false
+                user
+            };
+        } catch(e) {
+            return {
+                ok: false,
+                error: "User Not Found"
+            };
+        }
+    }
 }
